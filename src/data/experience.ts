1a7f641c7ceac21a566db180176a615ec3c909da
Mr-hariprasad <87761749+Mr-hariprasad@users.noreply.github.com>
2024-11-26 13:23:33 +0530

    add exp data

---
+++ src/data/experience.ts
@@ -0,0 +1,35 @@
+export const experience = [
+  {
+    title: 'Software Engineer Intern',
+    company: 'Tech Innovations Pvt. Ltd.',
+    duration: 'June 2023 - August 2023',
+    skills: [
+      'Python',
+      'PostgreSQL',
+      'Data Structures & Algorithms',
+      'Java',
+      'C#',
+      'Web Development',
+      'Networking',
+      'API Development',
+      'Version Control (Git)',
+    ],
+    current: true,
+  },
+  {
+    title: 'Junior Developer',
+    company: 'Innovative Solutions Ltd.',
+    duration: 'December 2022 - May 2023',
+    current: false,
+    skills: [
+      'HTML',
+      'C++',
+      'JavaScript',
+      'Basic Programming',
+      'Problem Solving',
+      'Database Management',
+    ],
+  },
+];
+
+export const experienceStatus = false;
