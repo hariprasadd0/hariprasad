f21b32b16fad17b49498ee4283f6b728bdbe3a1e
Mr-hariprasad <87761749+Mr-hariprasad@users.noreply.github.com>
2024-11-26 13:23:21 +0530

    add edu data

---
+++ src/data/education.ts
@@ -0,0 +1,24 @@
+export const eduData = [
+  {
+    title: 'Bachelor of Computer Applications',
+    company: 'ITM College of Arts & Science',
+    duration: '2021 - 2024',
+    skills: [
+      'Python',
+      'PSQL',
+      'Data Structures',
+      'Java',
+      'C#',
+      'Web',
+      'Networking',
+    ],
+    current: true,
+  },
+  {
+    title: 'XII-Computer Science',
+    company: 'G.H.S.S Malur',
+    duration: '2019-2020',
+    current: false,
+    skills: ['html', 'cpp', 'Basic CS'],
+  },
+];
