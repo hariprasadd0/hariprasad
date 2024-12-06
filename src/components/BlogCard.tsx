286a014b9a8721dec1ecdf3393b05d488d10ad4f
Mr-hariprasad <87761749+Mr-hariprasad@users.noreply.github.com>
2024-12-06 23:02:49 +0530

    add blog

--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -1,24 +1,13 @@
 import { Card } from './components';
-
-const BlogCard = () => {
-  const blog = [
-    {
-      id: 1,
-      title: 'How to Hack Your Brain',
-      description: 'What i did to fix my attention span',
-    },
-    {
-      id: 2,
-      title: 'How to Hack',
-      description: 'How did i create a ransomware',
-    },
-  ];
+import { Blog } from '@/data/blog';
+const BlogCard = ({ blog }) => {
   return (
     <div className="flex flex-col gap-2">
-      {blog.map((item) => (
+      {blog.map((item: Blog, index) => (
         <Card
-          className="p-2 rounded-sm w-full flex flex-col gap-1 hover:-translate-y-1 transition duration-200 ease-in-out"
-          key={item.id}
+          className="p-2 rounded-sm cursor-pointer w-full flex flex-col gap-1 hover:-translate-y-1 transition duration-200 ease-in-out"
+          key={index}
+          onClick={() => window.open(`${item.url}`, '_blank')}
         >
           <h1 className="font-semibold">{item.title}</h1>
           <span className="text-muted-foreground text-sm">
