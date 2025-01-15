b16e16dc4fddce2d79050145d706d69354f8688b
Mr-hariprasad <87761749+Mr-hariprasad@users.noreply.github.com>
2025-01-15 19:42:26 +0530

    update image imports

--- src/assets/workImg.ts
+++ src/assets/workImg.ts
@@ -2,8 +2,21 @@ import img1 from './images/1.webp';
 import img2 from './images/2.webp';
 import img3 from './images/5.webp';
 import EcoMingleLogo from './images/EcoMingle-logo.png';
-import Ecomingle from './images/Ecomingle-1.jpg';
+import Ecomingle01 from './images/EcoMingle01.webp';
+import Ecomingle02 from './images/Ecomingle02.webp';
+import Ecomingle03 from './images/EcoMingle03.webp';
+
 import FreelanceLogo from './images/Logo.png';
 import userLogo from './images/user.webp';
 
-export { img1, img2, img3, EcoMingleLogo, Ecomingle, FreelanceLogo, userLogo };
+export {
+  img1,
+  img2,
+  img3,
+  EcoMingleLogo,
+  Ecomingle01,
+  Ecomingle02,
+  Ecomingle03,
+  FreelanceLogo,
+  userLogo,
+};
--- src/data/works.ts
+++ src/data/works.ts
@@ -1,22 +1,32 @@
-import { img2, img3, EcoMingleLogo, Ecomingle } from '@/assets/workImg';
+import {
+  img2,
+  img3,
+  EcoMingleLogo,
+  Ecomingle01,
+  Ecomingle02,
+  Ecomingle03,
+} from '@/assets/workImg';
 
 const items = [
   {
     id: 1,
     avatarSrc: EcoMingleLogo,
-    carouselImg: Ecomingle,
-    avatarFallback: 'CN',
+    carouselImg: [Ecomingle01, Ecomingle02, Ecomingle03],
+    avatarFallback: 'EM',
     title: 'Ecomingle',
     context: 'Eco-conscious e-commerce platform',
     tech: ['React', 'Tailwind CSS'],
     icon: EcoMingleLogo,
     description:
       'A sustainable e-commerce platform empowering eco-conscious shopping by offering verified eco-friendly products and fostering greener choices for a better planet',
+    liveLink: 'https://eco-mingle.vercel.app/dashboard',
+    githubLink: 'https://github.com/hariprasadd0/EcoMingle',
   },
+
   {
     id: 2,
     avatarSrc: 'https://github.com/shadcn.png',
-    carouselImg: img2,
+    carouselImg: [img2],
     avatarFallback: 'CN',
     title: 'Bintray',
     context: 'Eco-conscious e-commerce platform',
@@ -24,11 +34,14 @@ const items = [
     icon: EcoMingleLogo,
     description:
       'Helped found the initial Booking.com Design System and its related services used on all Booking.com products. Currently, leading various efforts to further scale the system.',
+    liveLink: '',
+    githubLink: '',
   },
+
   {
     id: 3,
     avatarSrc: 'https://github.com/shadcn.png',
-    carouselImg: img3,
+    carouselImg: [img3],
     avatarFallback: 'CN',
     title: 'G-Cloud',
     context: 'Eco-conscious e-commerce platform',
@@ -36,6 +49,8 @@ const items = [
     icon: EcoMingleLogo,
     description:
       'Helped found the initial Booking.com Design System and its related services used on all Booking.com products. Currently, leading various efforts to further scale the system.',
+    liveLink: '',
+    githubLink: '',
   },
 ];
 
